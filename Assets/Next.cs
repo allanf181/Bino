1d3c2261a4b5db2cbb50b1f89f88e116f3f38244
Allan Fernando <allanfarmelin2001@yahoo.com.br>
2020-06-14 23:49:17 -0300

    Initial commit

---
+++ Assets/Next.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Next : MonoBehaviour
+{
+    public int scene;
+    public void LoadIndex(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
---
+++ Assets/Speed.cs
@@ -0,0 +1,48 @@
+﻿namespace Mapbox.Examples
+{
+	using Mapbox.Unity.Location;
+	using Mapbox.Utils;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	public class Speed : MonoBehaviour
+	{
+
+		[SerializeField]
+		Text _statusText;
+
+		private AbstractLocationProvider _locationProvider = null;
+		void Start()
+		{
+			if (null == _locationProvider)
+			{
+				_locationProvider = LocationProviderFactory.Instance.DefaultLocationProvider as AbstractLocationProvider;
+			}
+		}
+
+
+		void Update()
+		{
+			Location currLoc = _locationProvider.CurrentLocation;
+
+			if (currLoc.IsLocationServiceInitializing)
+			{
+				_statusText.text = "location services are initializing";
+			}
+			else
+			{
+				if (!currLoc.IsLocationServiceEnabled)
+				{
+					_statusText.text = "location services not enabled";
+				}
+				else
+				{
+					_statusText.text = currLoc.SpeedKmPerHour == null ? "0 Km/h" : string.Format("{0} Km/h", currLoc.SpeedKmPerHour.Value );
+				}
+			}
+
+		}
+	}
+}
